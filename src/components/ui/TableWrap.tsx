e24c464490ebaffd46aeee310cf2748d32680a4e
Wix Remote Machine <support@wix.com>
2026-01-27 10:55:31 +0000

    Add ResponsiveEmbed component

---
+++ src/components/ui/TableWrap.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TableWrap({
+  children,
+  className = "",
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return <div className={`table-wrap ${className}`}>{children}</div>;
+}
