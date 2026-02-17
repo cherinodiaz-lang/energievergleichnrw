d2d034b95aef1aeae17fc8f7adebc87d989a0e66
Wix Remote Machine <support@wix.com>
2026-02-17 17:13:23 +0000

    CMS-Setup für Blog

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -4,6 +4,60 @@
  */
 
 /**
+ * Collection ID: autoren
+ * Interface for Authors
+ */
+export interface Authors {
+  _id: string;
+  _createdDate?: Date;
+  _updatedDate?: Date;
+  /** @wixFieldType text */
+  authorName?: string;
+  /** @wixFieldType text */
+  authorBio?: string;
+  /** @wixFieldType image - Contains image URL, render with <Image> component, NOT as text */
+  authorPhoto?: string;
+  /** @wixFieldType text */
+  authorPosition?: string;
+  /** @wixFieldType text */
+  socialMediaLinks?: string;
+}
+
+
+/**
+ * Collection ID: blogposts
+ * Interface for BlogPosts
+ */
+export interface BlogPosts {
+  _id: string;
+  _createdDate?: Date;
+  _updatedDate?: Date;
+  /** @wixFieldType text */
+  title?: string;
+  /** @wixFieldType text */
+  urlSlug?: string;
+  /** @wixFieldType text */
+  category?: string;
+  /** @wixFieldType text */
+  author?: string;
+  /** @wixFieldType text */
+  excerpt?: string;
+  /** @wixFieldType image - Contains image URL, render with <Image> component, NOT as text */
+  thumbnail?: string;
+  /** @wixFieldType text */
+  content?: string;
+  /** @wixFieldType text */
+  tags?: string;
+  /** @wixFieldType date */
+  publishedDate?: Date | string;
+  /** @wixFieldType date */
+  updatedDate?: Date | string;
+  /** @wixFieldType number */
+  readingTime?: number;
+}
+
+
+/**
  * Collection ID: faq
  * Interface for HufiggestellteFragen
  */
@@ -46,6 +100,48 @@ export interface Informationsmaterial {
   thumbnail?: string;
   /** @wixFieldType date */
   publicationDate?: Date | string;
+}
+
+
+/**
+ * Collection ID: kategorien
+ * Interface for Categories
+ */
+export interface Categories {
+  _id: string;
+  _createdDate?: Date;
+  _updatedDate?: Date;
+  /** @wixFieldType text */
+  name?: string;
+  /** @wixFieldType text */
+  slug?: string;
+  /** @wixFieldType text */
+  description?: string;
+  /** @wixFieldType image - Contains image URL, render with <Image> component, NOT as text */
+  image?: string;
+  /** @wixFieldType number */
+  displayOrder?: number;
+}
+
+
+/**
+ * Collection ID: newsletterabonnenten
+ * Interface for NewsletterSubscribers
+ */
+export interface NewsletterSubscribers {
+  _id: string;
+  _createdDate?: Date;
+  _updatedDate?: Date;
+  /** @wixFieldType text */
+  email?: string;
+  /** @wixFieldType text */
+  firstName?: string;
+  /** @wixFieldType text */
+  lastName?: string;
+  /** @wixFieldType date */
+  subscribedDate?: Date | string;
+  /** @wixFieldType boolean */
+  consentGiven?: boolean;
 }
 
 
