f1861b93eb5bedde32d3127aca3e85fd28334091
Wix Remote Machine <support@wix.com>
2026-02-21 02:27:03 +0000

    CONTACT config erstellen

---
+++ src/config/contact.ts
@@ -0,0 +1,11 @@
+export const CONTACT = {
+  email: "support@energievergleich.nrw",
+  phone: "+49 156 78556004",
+  addressLines: [
+    "ENERGIEVERGLEICH NRW (P. Kohmann & A. Danos)",
+    "Wasserstr. 48",
+    "33378 Rheda-Wiedenbrück",
+    "Deutschland",
+  ],
+  website: "https://www.energievergleich.shop",
+} as const;
